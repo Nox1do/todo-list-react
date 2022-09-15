1b00cb12ae51bcfd52ee095486c9a7759a6dcd7a
Nox1do <noogux@gmail.com>
2022-09-15 02:35:14 +0000

    updating input from todos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,32 +10,42 @@ const [inputValue, setInputValue] = useState("")
 const [todos, setTodos] = useState([])
 
   return (
+	<div className="justify-content-center">
     <div className="todo container d-flex justify-content-center">
-		
+	
       <ul className="flex-column">
-	  <h3>My Todos</h3>
+	  <h3 className="text-center"  >My Todos</h3>
         <input type="text" required onChange={(event)=> setInputValue(event.target.value)}
 		value={inputValue} 
 		onKeyPress={(enter) => {
-		if (enter.key === "Enter") { setTodos(todos.concat(inputValue))
+			if(inputValue === "") {
+				return;
+		}
+		if (enter.key === "Enter") { 
+		setTodos(todos.concat(inputValue))
 	    setInputValue("") 
+	
+	
 	}
+	
 }}
-		 placeholder="type text here"/>
+		 placeholder="Type text here"/>
         
         {todos.map((item, index) => (
 		<li> <strong>{item}</strong>
-		<button onClick={() =>
+		<button className="float-end" onClick={() =>
 			setTodos(todos.filter((t, currentIndex)=> index != currentIndex))}>Borrar</button>
 		</li>
 		
 		
 		))} 
 		
-		<li><strong> {todos.length} </strong> Items</li>		
+				
        
-      </ul>  	  
+      </ul>
+	  
     </div>
+	<div className="todos text-center container d-flex justify-content-center"><strong> {todos.length} </strong>Items</div></div>	 
 	
 	
 	
