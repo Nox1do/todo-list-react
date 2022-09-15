32655e7b64ec866d622c4cfaea1ccaede0e6cb33
Nox1do <noogux@gmail.com>
2022-09-15 02:53:24 +0000

    fixed bug when user type space in the input returns a empty value

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,53 +2,54 @@ import React from "react";
 import { useState } from "react";
 //include images into your bundle
 
-
 //create your first component
 const Home = () => {
-
-const [inputValue, setInputValue] = useState("")
-const [todos, setTodos] = useState([])
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
   return (
-	<div className="justify-content-center">
-    <div className="todo container d-flex justify-content-center">
-	
-      <ul className="flex-column">
-	  <h3 className="text-center"  >My Todos</h3>
-        <input type="text" required onChange={(event)=> setInputValue(event.target.value)}
-		value={inputValue} 
-		onKeyPress={(enter) => {
-			if(inputValue === "") {
-				return;
-		}
-		if (enter.key === "Enter") { 
-		setTodos(todos.concat(inputValue))
-	    setInputValue("") 
-	
-	
-	}
-	
-}}
-		 placeholder="Type text here"/>
-        
-        {todos.map((item, index) => (
-		<li> <strong>{item}</strong>
-		<button className="float-end" onClick={() =>
-			setTodos(todos.filter((t, currentIndex)=> index != currentIndex))}>Borrar</button>
-		</li>
-		
-		
-		))} 
-		
-				
-       
-      </ul>
-	  
+    <div className="justify-content-center">
+      <div className="todo container d-flex justify-content-center">
+        <ul className="flex-column">
+          <h3 className="text-center">My Todos</h3>
+          <input
+            type="text"
+            required
+            onChange={(event) => setInputValue(event.target.value)}
+            value={inputValue}
+            onKeyPress={(enter) => {
+              if (inputValue.trim() === "") {
+                return;
+              }
+              if (enter.key === "Enter") {
+                setTodos(todos.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+            placeholder="Type text here"
+          />
+
+          {todos.map((item, index) => (
+            <li>
+             <strong>{item}</strong>
+              <button
+                className="float-end"
+                onClick={() =>
+                  /*filter para funcion borrar al clickear*/
+                  setTodos(
+                    todos.filter((t, currentIndex) => index != currentIndex)
+                  )
+                }
+              >
+                Borrar
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="todos text-center container d-flex justify-content-center">
+        <p><strong> {todos.length} </strong>Items</p>  </div>
     </div>
-	<div className="todos text-center container d-flex justify-content-center"><strong> {todos.length} </strong>Items</div></div>	 
-	
-	
-	
   );
 };
 
